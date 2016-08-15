3dec5fd54028b96c296f3bee469788cfcde5da62
princessbeyonce <af6244@bard.edu>
2016-08-15 10:29:38 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -29,7 +29,7 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "Hey gurl, did you hear that Lemonade album?"
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
